dde976eb0bba43c0c34de2d64468f95839d78387
Ivan Garcia <ivangarcia@indra.es>
2018-01-18 14:34:45 +0100

    Simple REST service

---
+++ src/main/java/businesskeepertest/businesskeepertest/BusinessKeeperTestApplication.java
@@ -0,0 +1,12 @@
+package businesskeepertest.businesskeepertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessKeeperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessKeeperTestApplication.class, args);
+	}
+}
